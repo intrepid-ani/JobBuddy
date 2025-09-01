62a77ecd750b4e82774664c4b4bd0cf27b4ab1e5
Ani <intrepidani05@gmail.com>
2025-09-01 16:33:50 +0530

    fix: update API endpoints to use the correct backend URLs

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,8 +1,8 @@
 export const USER_API_END_POINT =
-  "https://job-buddy-three.vercel.app/api/v1/user";
+  "https://job-buddy-backend.vercel.app/api/v1/user";
 export const JOB_API_END_POINT =
-  "https://job-buddy-three.vercel.app/api/v1/job";
+  "https://job-buddy-backend.vercel.app/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "https://job-buddy-three.vercel.app/api/v1/application";
+  "https://job-buddy-backend.vercel.app/api/v1/application";
 export const COMPANY_API_END_POINT =
-  "https://job-buddy-three.vercel.app/api/v1/company";
+  "https://job-buddy-backend.vercel.app/api/v1/company";
