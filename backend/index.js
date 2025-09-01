966fdfac60f2b0399feb8eb30e2fc9ea13171ded
Ani <intrepidani05@gmail.com>
2025-09-01 16:25:37 +0530

    fix: simplify CORS configuration and enhance MongoDB connection logging

--- backend/index.js
+++ backend/index.js
@@ -16,12 +16,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-  origin: "https://job-buddy-9bc5.vercel.app",
-  credentials: true,
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
--- backend/utils/db.js
+++ backend/utils/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const res = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(res.connection.host);
+    console.log(res.connection.host, res.connection.name);
     console.log("mongodb connected successfully");
   } catch (error) {
     console.log(error);
